5e3466aae8fb5bf786ca8c5f98507adb94ecd982
triosakti <112994326+Triosakt@users.noreply.github.com>
2022-09-22 19:28:51 +0700

    Add files via upload

---
+++ TRIO SAKTI ARDIKA_2257051012_Praktest2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using  namespace std;
+int main(){
+	
+	int angka;
+	cout<<"masukan angka:";
+	cin>>angka;
+	if(angka==1){
+		cout<<"satu\n";
+	}else
+	if(angka==2){
+		cout<<"dua\n";
+		
+	}else
+	if(angka==3){
+		cout<<"tiga\n";
+		
+	}else
+	if (angka>3){
+		cout<<"bilangan lebih dari tiga\n";
+		
+	
+		return 0;
+	}
+	
+}
